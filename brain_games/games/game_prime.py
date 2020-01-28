31da35a02170d758517b475de94c24c04ead5420
Sergey Demikhov <demikhov.sergey@gmail.com>
2020-01-28 13:51:06 -0600

    add new game "prime"

---
+++ brain_games/games/game_prime.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+"""Contain game-prime logic."""
+
+from random import randrange
+
+from .game_base import game_base
+
+
+game_prime_description = """
+Answer "yes" if given number is prime. Otherwise answer "no"
+"""
+
+
+def game_prime_logic():
+    """Define logic for brain-prime game.
+
+    return tuple of question, correct answer
+    """
+    question = randrange(100)
+    answer = 'yes'
+    if question <= 1:
+        answer = 'no'
+    elif question >= 4:
+        for divider in range(2, question):
+            if question % divider == 0:
+                answer = 'no'
+                break
+    return question, str(answer)
+
+
+def game_prime():
+    """Passes description and logic for brain-prime game to engine."""
+    game_base(game_prime_description, game_prime_logic)
---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+
+"""Run game 'prime'"""
+
+from ..games.game_prime import game_prime
+
+
+def main():
+    """Entry point for game."""
+    game_prime()
+
+
+if __name__ == '__main__':
+    main()
